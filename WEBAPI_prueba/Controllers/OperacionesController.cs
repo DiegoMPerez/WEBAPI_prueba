8546d4480561b935b1720b690bdcfa74329ade36
DIEGO MANUEL PEREZ CARVAJAL <dperez@Farmaenlace.com>
2024-01-28 21:46:51 -0500

    Agregar archivos de proyecto.

---
+++ WEBAPI_prueba/Controllers/OperacionesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using RoutePrefixAttribute = System.Web.Http.RoutePrefixAttribute;
+
+namespace WEBAPI_prueba.Controllers
+{
+    [RoutePrefix("api/Operaciones")]
+    public class OperacionesController : ApiController
+    {
+        // GET: Operaciones
+        [HttpGet]
+        public  string Suma(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+    }
+}
